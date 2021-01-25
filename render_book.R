bf1351714d63843fe0604f13a4e4be0b633bd5f6
nehamoopen <neha.moopen@gmail.com>
2021-01-25 13:50:05 +0100

    update GH workflow

--- render_book.R
+++ render_book.R
@@ -20,3 +20,6 @@ bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 bookdown::render_book("index.Rmd", "bookdown::epub_book")
 
+# download GitHub Actions workflow
+
+usethis::use_github_action(url = "https://raw.githubusercontent.com/ropenscilabs/actions_sandbox/master/.github/workflows/deploy_bookdown.yml")
